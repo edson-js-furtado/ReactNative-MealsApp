65a608b450d97eeac199fd25b257195a318eb46c
Edson Furtado <119877699+edson-js-furtado@users.noreply.github.com>
2023-02-01 12:47:36 +0000

    components

---
+++ components/CategoryGridTitle.js
@@ -0,0 +1,46 @@
+import { Pressable, View, Text, StyleSheet } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+function CategoryGridTitle({ title, color, pressed }) {
+  const navigation = useNavigation();
+  
+  return (
+    <View style={[styles.gridItem, { backgroundColor: color }]}>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={styles.button}
+        onPress={pressed}
+      >
+        <View style={styles.innerContainer}>
+          <Text style={styles.title}>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+export default CategoryGridTitle;
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    elevation: 4,
+    overflow: "hidden",
+  },
+  button: {
+    flex: 1,
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
---
+++ components/MealList/MealList.js
@@ -0,0 +1,36 @@
+import { View, FlatList, StyleSheet } from "react-native";
+import MealItem from "./MealItem";
+
+function MealList({item}) {
+  function renderMealItem(itemData) {
+    return (
+      <MealItem
+        id={itemData.item.id}
+        title={itemData.item.title}
+        imageUrl={itemData.item.imageUrl}
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity}
+        affordability={itemData.item.affordability}
+      />
+    );
+  }
+  //It is the main return
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={item}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
+    </View>
+  );
+}
+
+export default MealList;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
