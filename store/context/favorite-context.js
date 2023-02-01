ad74168203cdea8b646df2de824bc01bb06bb2cd
Edson Furtado <119877699+edson-js-furtado@users.noreply.github.com>
2023-02-01 12:46:32 +0000

    store

---
+++ store/context/favorite-context.js
@@ -0,0 +1,35 @@
+import { createContext, useState } from "react";
+
+export const FavoriteContext = createContext({
+  ids: [],
+  addFavorite: (id) => {},
+  removeFavorite: (id) => {}
+});
+
+function FavoritesContextProvider({ children }) {
+  const [favoriteMealIds, setFavoriteMealsIds] = useState([]);
+
+  function addFavorite(id) {
+    setFavoriteMealsIds((currentFavIds) => [...currentFavIds, id]);
+  }
+
+  function removeFavorite(id) {
+    setFavoriteMealsIds((currentFavIds) =>
+      currentFavIds.filter((mealId) => mealId !== id)
+    );
+  }
+
+  const value = {
+    ids: favoriteMealIds,
+    addFavorite: addFavorite,
+    removeFavorite: removeFavorite,
+  };
+
+  return (
+    <FavoriteContext.Provider value={value}>
+      {children}
+    </FavoriteContext.Provider>
+  );
+}
+
+export default FavoritesContextProvider;
